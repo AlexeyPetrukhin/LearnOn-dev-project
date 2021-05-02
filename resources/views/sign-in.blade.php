a5d3136a4d1abf965dc7623d1412c77bf987a2f0
Alexey <paa336528@gmail.com>
2021-05-02 15:33:22 +0700

    add model "BlogPost"; add migration "BlogPost"; add view "sign-in"

--- resources/views/sign-in.blade.php
+++ resources/views/sign-in.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRM</title>
-    <link rel="stylesheet" href="{{ asset("css/style.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/sign-in/style.css") }}">
 </head>
 <body>
 <div class="wrapper">
